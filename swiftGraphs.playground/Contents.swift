5b61f477d5ee6ff841c36a729fc42562798aa7c3
Nestor Hernandez <nestor.jbautista@gmail.com>
2018-10-16 18:18:58 -0500

    dfs, bfs added

--- swiftGraphs.playground/Contents.swift
+++ swiftGraphs.playground/Contents.swift
@@ -1,10 +1,11 @@
 import UIKit
 
 var str = "Hello, playground"
-struct Node {
+class Node {
     
     var value: Int?
     var edges: [Edge]
+    var visited = false
     
     init(value: Int?) {
         self.value = value
@@ -12,7 +13,7 @@ struct Node {
     }
 }
 
-struct Edge {
+class Edge {
     
     var value: Int?
     var nodeFrom: Node?
@@ -154,11 +155,60 @@ class Graph {
         toFound?.edges.append(newEdge)
         edges.append(newEdge)
     }
+    
+    
+
+
+    func dfs(_ startNode: Node) -> [Int] {
+        let visited = [startNode.value!]
+        
+        return dfsHelper(startNode, visited: visited)
+    }
+    
+    // Create an iterative implementation.
+    // Return a list of the node values.
+    func bfs(_ startNode: Node) -> [Int] {
+        var visited: [Int] = []
+        var toVisit: [Node] = [] // Treat this array as a queue
+        
+        toVisit.append(startNode)
+        visited.append(startNode.value!)
+        startNode.visited = true
+        
+        while !toVisit.isEmpty {
+            let node = toVisit.removeFirst()
+            
+            for edgeObject in node.edges {
+                let neighborNode = edgeObject.nodeTo
+                
+                if !(neighborNode?.visited)! {
+                    toVisit.append(neighborNode!)
+                    neighborNode?.visited = true
+                    visited.append((neighborNode?.value)!)
+                }
+            }
+        }
+        return visited
+    }
+    
+    // helper method - use to implement DFS recursively
+    func dfsHelper(_ current: Node, visited: [Int]) -> [Int] {
+        var result = visited
+        
+        for edgeObject in current.edges {
+            if !result.contains((edgeObject.nodeTo?.value)!) {
+                result.append(edgeObject.nodeTo!.value!)
+                result = dfsHelper(edgeObject.nodeTo!, visited: result)
+            }
+        }
+        return result
+    }
+    
+ 
 }
 
 // Test cases
-let node = Node(value: 1)
-let graph = Graph(nodes: [node], edges: [])
+let graph = Graph(nodes: [], edges: [])
 graph.insertEdgeWithValue(100, nodeFromValue: 1, nodeToValue: 2)
 graph.insertEdgeWithValue(101, nodeFromValue: 1, nodeToValue: 3)
 graph.insertEdgeWithValue(102, nodeFromValue: 1, nodeToValue: 4)
@@ -167,3 +217,9 @@ graph.insertEdgeWithValue(103, nodeFromValue: 3, nodeToValue: 4)
 print(graph.getEdgeList()) // Should be [(100, 1, 2), (101, 1, 3), (102, 1, 4), (103, 3, 4)]
 print(graph.getAdjacencyList()) // Should be [[], [(2, 100), (3, 101), (4, 102)], [], [(4, 103)], []]
 print(graph.getAdjacencyMatrix()) // Should be [[0, 0, 0, 0, 0], [0, 0, 100, 101, 102], [0, 0, 0, 0, 0], [0, 0, 0, 0, 103], [0, 0, 0, 0, 0]]
+
+
+print(graph.dfs(graph.nodes[0])) // Should be [1, 2, 3, 4]
+print(graph.bfs(graph.nodes[0])) // Should be [1, 2, 3, 4]
+
+
